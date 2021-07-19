c0e95147af9aef3a49e606e6bfceb11996b070a4
Mark Angus <mark.angus@calytrix.com>
2021-07-19 21:58:44 +0800

    Lesson 3 complete
    Working: ABC-123  (change to Fixes: ABC-123 if you want for fixes)

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
+            image: './assets/images/socks_green.jpg',
             description: 'Some dam nice shocks'
         }
     }
