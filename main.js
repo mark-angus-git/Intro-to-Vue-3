a735af4fc3d5cfa8d2826e4a71e25603887e148f
Mark Angus <mark.angus@calytrix.com>
2021-07-21 21:59:47 +0800

    Lists added
    Longer description if necessary. Describe the why if it is important. Couple paragraphs is a good max limit. Skip if not needed
    
    Working: ABC-123  (change to Fixes: ABC-123 if you want for fixes)

--- main.js
+++ main.js
@@ -3,7 +3,12 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             image: './assets/images/socks_green.jpg',
-            description: 'Some dam nice shocks'
+            description: 'Some dam nice shocks',
+            url: 'file:///F:/VUE3%20Projects/Intro-to-Vue-3/index.html',
+            inventory: 2,
+            onSale: true,
+            inStock: true,
+            details: ['50% cotton', '30% wool', '20% polyester']
         }
     }
 })
