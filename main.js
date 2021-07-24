28d896c07be98129e6af68bed7d98e0bc524476e
Mark Angus <mark.angus@calytrix.com>
2021-07-24 20:39:34 +0800

    Completed more lessons
    Longer description if necessary. Describe the why if it is important. Couple paragraphs is a good max limit. Skip if not needed
    
    Working: ABC-123  (change to Fixes: ABC-123 if you want for fixes)

--- main.js
+++ main.js
@@ -1,14 +1,31 @@
 const app = Vue.createApp({
     data() {
         return {
+            cart:0,
             product: 'Socks',
             image: './assets/images/socks_green.jpg',
             description: 'Some dam nice shocks',
             url: 'file:///F:/VUE3%20Projects/Intro-to-Vue-3/index.html',
             inventory: 2,
             onSale: true,
-            inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            inStock: false,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg'},
+            ],
+            sizes: ['small','medium','large'],
         }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        removeFromCart() {
+            this.cart -= 1
+        },
+        updateImage(variantImage) {
+            this.image = variantImage
+        },
     }
 })
