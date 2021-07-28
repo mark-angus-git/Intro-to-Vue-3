103d728ee2d94149a91a4034ac01ece9bdbfb770
Mark Angus <mark.angus@calytrix.com>
2021-07-28 21:24:58 +0800

    WIP
    Longer description if necessary. Describe the why if it is important. Couple paragraphs is a good max limit. Skip if not needed
    
    Working: ABC-123  (change to Fixes: ABC-123 if you want for fixes)

--- main.js
+++ main.js
@@ -3,16 +3,16 @@ const app = Vue.createApp({
         return {
             cart:0,
             product: 'Socks',
-            image: './assets/images/socks_green.jpg',
+            brand: 'Vue Mastery',
+            selectVariant: 0,
             description: 'Some dam nice shocks',
             url: 'file:///F:/VUE3%20Projects/Intro-to-Vue-3/index.html',
             inventory: 2,
             onSale: true,
-            inStock: false,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg'},
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0},
             ],
             sizes: ['small','medium','large'],
         }
@@ -24,8 +24,19 @@ const app = Vue.createApp({
         removeFromCart() {
             this.cart -= 1
         },
-        updateImage(variantImage) {
-            this.image = variantImage
+        updateVariant(index) {
+            this.selectedVariant = index
+        },
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
         },
     }
 })
